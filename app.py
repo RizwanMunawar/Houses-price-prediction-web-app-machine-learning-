e893360d72cc73476d0ffce8b88f7bf700afdb88
Muhammad Rizwan Munawar <62513924+RizwanMunawar@users.noreply.github.com>
2020-09-12 18:32:31 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = flask.Flask(__name__, template_folder='templates')
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
+	#main code
 	if flask.request.method == 'GET':
 		return(flask.render_template('index.html')) 
 	if flask.request.method == 'POST':
