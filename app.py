6566386e73b520b3d3fda02d7bca288056c8b098
Muhammad Rizwan Munawar <62513924+RizwanMunawar@users.noreply.github.com>
2020-09-12 17:55:44 +0500

    Update app.py

--- app.py
+++ app.py
@@ -18,21 +18,9 @@ def main():
         aan = flask.request.form['aan']
         aanb = flask.request.form['aanb']
         ap = flask.request.form['ap']
-
-        input_variables = pd.DataFrame([[aai,aah,aan,aanb,ap]],
-                                       columns=['Avg. Area Income','Avg. Area House Age','Avg. Area Number of Rooms','Avg. Area Number of Bedrooms','Area Population'],
-                                       dtype=float,
-                                       index=['input'])
+        input_variables = pd.DataFrame([[aai,aah,aan,aanb,ap]],columns=['Avg. Area Income','Avg. Area House Age','Avg. Area Number of Rooms','Avg. Area Number of Bedrooms','Area Population'],dtype=float,index=['input'])
         prediction = model.predict(input_variables)[0]
-	return flask.render_template('index.html',
-                                     original_input={'Avg. Area Income':aai,
-                                                     'Avg. Area House Age':aah,
-                                                     'Avg. Area Number of Rooms':aan,
-			         								'Avg. Area Number of Bedrooms':aanb,
-			         								'Area Population':ap	
-				},
-                                     result=prediction,
-                                     )
+	return flask.render_template('index.html',original_input={'Avg. Area Income':aai,'Avg. Area House Age':aah,'Avg. Area Number of Rooms':aan,'Avg. Area Number of Bedrooms':aanb,'Area Population':ap},result=prediction)
 
 if __name__ == '__main__':
     app.run()
